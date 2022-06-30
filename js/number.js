fe0986b3919f311fbc7ac706803d9e292932a808
ugne <ugnetomkute@gmail.com>
2022-06-30 23:19:39 +0300

    numbers

---
+++ js/number.js
@@ -0,0 +1,60 @@
+console.clear()
+/*
+normalus skaiciai
+- sveikieji
+- desimtainiai
+- teigiami/neigiami
+
+nenormalus skaiciai
+- infinity / -infinity
+- NaN (not-a-number)
+
+Kintamuju inicijavimas
+- const (reiksme negali buti pakeista/default)
+- let (leidziame keisti reiksme)
+- var (nenaudoti jei nenori tureti problemu)
+
+*/
+
+
+console.log(5555);
+
+const metai = 2022
+console.log(metai);
+console.log(2022);
+
+const PI = 3.14
+console.log(PI)
+
+const gimimometai = 1984
+console.log(gimimometai)
+
+const minus = -77
+console.log(minus)
+
+//NAMU DARBAS
+/* Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+const geguze = 5;
+const birzelis = 6;
+const liepa = 7;
+
+console.log(geguze);
+console.log(birzelis);
+console.log(liepa);
+console.log(liepa);
+
+const suo = rufis;
+const kate = sakura;
+const zuvis = auksine;
+
+console.log(kate);
+console.log(suo);
+
+console.log('----------------')
+
+const pinigine = 0;
+console.log(geguze);
+console.log(geguze);
