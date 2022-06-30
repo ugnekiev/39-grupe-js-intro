8e1c526aaa060fc7be7832bd2a8038e006146595
ugne <ugnetomkute@gmail.com>
2022-06-30 16:06:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//single line comment
+
+/* multi
+    line
+    comment */
+
+console.log(64653);
+console.log(1);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(3);
+console.log(4);
+
+
