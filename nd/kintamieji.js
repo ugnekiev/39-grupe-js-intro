d06b7a0aa8fafc50465bf2972221963e6df5323d
ugne <ugnetomkute@gmail.com>
2022-07-04 17:00:42 +0300

    Kintamieji nd

---
+++ nd/kintamieji.js
@@ -0,0 +1,21 @@
+console.clear();
+const atlyginimas = 3000;
+console.log(atlyginimas);
+
+const pusmecioAtlyginimas = 3000 * 6;
+console.log(pusmecioAtlyginimas)
+
+const ziemosTemperatura = -15
+console.log(ziemosTemperatura)
+
+console.log(`------------------`)
+
+const sukis = "mokytis yra nuostabu";
+console.log(sukis)
+
+const sukis1 = `mokytis
+yra 
+nuostabu`;
+console.log(sukis1)
+
+
